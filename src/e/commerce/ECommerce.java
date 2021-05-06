d6856176143f665f5e8ed259e72e23d990fbf152
nehadubey20 <008nehadubey@gmail.com>
2021-05-06 10:12:21 +0530

    changes in front page

--- src/e/commerce/ECommerce.java
+++ src/e/commerce/ECommerce.java
@@ -17,7 +17,7 @@ public class ECommerce {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int browse;
         
 	int choice;
         Login login = new Login();
